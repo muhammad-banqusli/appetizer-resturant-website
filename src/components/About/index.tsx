a057f09bae003c4ff1f0ab02bc9e1487e2b089a8
--replace-all <mohamad.banksli@ogr.dpu.edu.tr>
2024-08-21 16:56:47 +0300

    fixed p text in About

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -19,7 +19,7 @@ const About = () => {
                             Little Blind Text, that where it came from it would
                             have been rewritten a thousand times and everything
                             that was left from its origin would be the word
-                            "and" and the Little Blind Text should turn around
+                            and the Little Blind Text should turn around
                             and return to its own, safe country. A small river
                             named Duden flows by their place and supplies it
                             with the necessary regelialia. It is a paradisematic
